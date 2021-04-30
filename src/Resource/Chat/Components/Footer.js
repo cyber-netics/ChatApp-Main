b64784e2046680ee1ce45e6d848f64a5d92c20de
Eduard  Jacobs <eduardjacobs@Eduards-MacBook-Pro.local>
2021-04-30 01:35:10 -0700

    chat footer cleanup

--- src/Resource/Chat/Components/Footer.js
+++ src/Resource/Chat/Components/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-
 import Btn from 'Components/Button';
 import { Input } from 'Components/SharedStyles';
 import {
@@ -35,17 +34,17 @@ const Form = styled.form`
   width: 100%;
 `;
 
+const InputMessage = styled(Input)`
+  padding: 11px 15px;
+  width: 100%;
+`;
+
 const Button = styled(Btn)`
   padding: 10px 15px;
   background: ${(props) =>
     props.type
       ? props.theme.colors[props.type]
       : 'none'};
-`;
-
-const InputMessage = styled(Input)`
-  padding: 11px 15px;
-  width: 100%;
 `;
 
 const Footer = () => {
@@ -69,21 +68,18 @@ const Footer = () => {
               icon={'Smile'}
             />
           </ListItem>
-
           <ListItem className="inputmessage">
             <InputMessage
               onChange={handleSelectFile}
               placeholder="write a message"
             />
           </ListItem>
-
           <ListItem>
             <Button
               onClick={handleSelectFile}
               icon={'Paperclip'}
             />
           </ListItem>
-
           <ListItem>
             <Button
               icon={'Send'}
